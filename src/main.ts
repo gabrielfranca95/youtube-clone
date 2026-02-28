3bf01b68677eed05672172f296bdf759153b60c7
gabrielfranca95 <gabrielfranca95@users.noreply.github.com>
2026-02-28 10:15:00 -0300

    build: configure pinia state management

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 ﻿import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import { createPinia } from 'pinia'
 import './style.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 app.use(vuetify)
+app.use(pinia)
 app.mount('#app')
