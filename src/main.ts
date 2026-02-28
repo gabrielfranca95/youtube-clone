77acc5c90db63acca82384fbc0f28982a5160ca9
gabrielfranca95 <gabrielfranca95@users.noreply.github.com>
2026-02-28 11:00:00 -0300

    build: setup vue router

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-﻿import { createApp } from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './style.css'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
 
 const app = createApp(App)
-const pinia = createPinia()
+
+app.use(createPinia())
+app.use(router)
 app.use(vuetify)
-app.use(pinia)
+
 app.mount('#app')
---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/video/:id',
+      name: 'video-detail',
+      component: () => import('../views/VideoDetailView.vue')
+    }
+  ],
+  scrollBehavior(_to, _from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
+})
+
+export default router
